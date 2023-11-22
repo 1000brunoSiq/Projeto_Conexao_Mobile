d290e2f65da268976cdf1b081ac1dfbff773fb51
Bruno Siqueira dos Anjo <brunosiq_anjos@hotmail.com>
2023-11-22 17:05:24 -0300

    removed folders front-end  and back-end

--- testLogin.php
+++ testLogin.php
@@ -3,7 +3,7 @@ session_start();
 // print_r($_REQUEST);
 if (isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha'])) {
     //Acessa
-    include_once('config.php');
+    include_once('/Back-end/config.php');
     $email = $_POST['email'];
     $senha = $_POST['senha'];
 
@@ -21,15 +21,15 @@ if (isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha']
     if (mysqli_num_rows($result) < 1) {
         unset($_SESSION['email']);
         unset($_SESSION['senha']);
-        header('Location: login.php');
+        header('Location: /Front-end/area_do_colaborador.html');
     } else {
 
         $_SESSION['email'] = $email;
         $_SESSION['senha'] = $senha;
 
-        header('Location: sistema.php');
+        header('Location: /Back-end/sistema.php');
     }
 } else {
     // Não acessa
-    header('Location: login.php');
+    header('Location: /Front-end/area_do_colaborador.html');
 }
