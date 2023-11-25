4ea911f7204ed09126f53b5ab7db65b9c9d13531
Bruno Siqueira dos Anjo <brunosiq_anjos@hotmail.com>
2023-11-25 16:11:53 -0300

    updates

--- sistema.php
+++ sistema.php
@@ -2,11 +2,11 @@
 session_start();
 include_once('config.php');
 // print_r($_SESSION);
-if ((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true)) {
-    unset($_SESSION['email']);
-    unset($_SESSION['senha']);
-    header('Location: login.php');
-}
+// if ((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true)) {
+//     unset($_SESSION['email']);
+//     unset($_SESSION['senha']);
+//     header('Location: login.php');
+// }
 $logado = $_SESSION['email'];
 
 $sql = "SELECT * FROM produtos ORDER BY id DESC";
