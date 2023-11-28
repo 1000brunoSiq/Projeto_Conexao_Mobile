37926d54c4a2942d87aec75999aeb61f92fc16a1
Barbara <130870960+collotebarbara@users.noreply.github.com>
2023-11-28 18:34:29 -0300

    pagina de pagamento

--- main.js
+++ main.js
@@ -24,3 +24,25 @@ userIcon.addEventListener('click', function() {
   userIcon.classList.add('invi');
   cartIcon.classList.add('invi');
 });
+
+
+
+document.getElementById("metodo_pagamento").addEventListener("change", function () {
+  var metodoPagamento = this.value;
+
+  // Esconder todos os campos específicos
+  document.getElementById("cartao_campos").style.display = "none";
+  document.getElementById("boleto_campos").style.display = "none";
+  document.getElementById("pix_campos").style.display = "none";
+
+  // Mostrar os campos específicos do método selecionado
+  if (metodoPagamento === "cartao") {
+      document.getElementById("cartao_campos").style.display = "block";
+  } else if (metodoPagamento === "boleto") {
+      document.getElementById("boleto_campos").style.display = "block";
+  } else if (metodoPagamento === "pix") {
+      document.getElementById("pix_campos").style.display = "block";
+  }
+});
+
+
