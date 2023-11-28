3422891fbde89222d34b8ab0cbf55ee01bce3aa8
Bruno Siqueira dos Anjo <brunosiq_anjos@hotmail.com>
2023-11-27 23:00:53 -0300

    updates

--- cadastro_cliente.php
+++ cadastro_cliente.php
@@ -1,35 +1,55 @@
 <?php
 session_start();
-include ('config.php');
+include('config.php');
 
-$nome = mysqli_real_escape_string($conexao,trim($_POST['nome']));
-$celular = mysqli_real_escape_string($conexao,trim($_POST['celular']));
-$email = mysqli_real_escape_string($conexao,trim($_POST['email']));
-$senha = mysqli_real_escape_string($conexao,trim(md5($_POST['senha'])));
-$endereco = mysqli_real_escape_string($conexao,trim($_POST['endereco']));
+if (isset($_SESSION['cadastro_sucesso']) && $_SESSION['cadastro_sucesso'] === true) {
+    echo '<div class="alert alert-success">';
+    echo 'Cadastro realizado com sucesso!';
+    echo '</div>';
+    unset($_SESSION['cadastro_sucesso']);
+}
 
-$sql = "SELECT COUNT(*) AS total FROM cadastro_clientes WHERE email = '$email'";
-$result = mysqli_query($conexao,$sql);
-$row = mysqli_fetch_assoc($result);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Outros campos do formulário...
 
-if($row['total'] == 1 ){
-    $_SESSION['usuario_existe'] = true;
-    header('location: cadastro.php?erro=Email já cadastrado. Escolha outro email.');
-    exit;
+    if (empty($nome) || empty($celular) || empty($email) || empty($senha) || empty($endereco)) {
+        $_SESSION['erro'] = "Formulários vazios. Preencha os campos para validação do cadastro.";
+        // header("Location: cadastro.php");
+        // exit();
+    }
+
+
+
+    $nome = mysqli_real_escape_string($conexao, trim($_POST['nome']));
+    $celular = mysqli_real_escape_string($conexao, trim($_POST['celular']));
+    $email = mysqli_real_escape_string($conexao, trim($_POST['email']));
+    $senha = mysqli_real_escape_string($conexao, trim(md5($_POST['senha'])));
+    $endereco = mysqli_real_escape_string($conexao, trim($_POST['endereco']));
+
+    $sql = "SELECT COUNT(*) AS total FROM cadastro_clientes WHERE email = '$email'";
+    $result = mysqli_query($conexao, $sql);
+    $row = mysqli_fetch_assoc($result);
+
+    if ($row['total'] == 1) {
+        $_SESSION['usuario_existe'] = "Email já cadastrado. Escolha outro email.";
+        header('location: cadastro.php');  // Redireciona para a página de cadastro
+        exit;
+    }
 }
 $sql = "INSERT INTO cadastro_clientes (nome, celular, email, senha, endereco) VALUES ('$nome', '$celular', '$email', '$senha', '$endereco')";
 
-if($conexao->query($sql) === true){
-    $_SESSION['status_cadastro'] = true;
-}else {
+if ($conexao->query($sql) === true) {
+    // $_SESSION['status_cadastro'] = true;
+    $_SESSION['cadastro_sucesso'] = "Cadastro realizado com sucesso!";
+    header('location: cadastro.php');
+    exit;
+} else {
     $_SESSION['status_cadastro'] = false;
     $_SESSION['error'] = "Erro ao cadastrar o usuário: " . $conexao->error;
 }
 
+
 $conexao->close();
-
-
 
 header('location: cadastro.php');
 exit;
-?>
--- login_cliente.php
+++ login_cliente.php
@@ -1,31 +1,38 @@
 <?php
-include ('config.php');
+include('config.php');
 
 session_start();
-if (empty($_POST) || empty($_POST["email"]) || empty($_POST["senha"])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
 
-    header('Location: cadastro.php?erro=Campos email e senha são obrigatórios!');
-    exit;
-}
+    if (empty($email) || empty($senha)) {
+        $_SESSION['erro'] = "Campos email e senha são obrigatórios!";
+        // header("Location: cadastro.php");
+        // exit();
+    }
 
-$email = $_POST['email'];
-$senha = $_POST['senha'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
 
-$sql = "SELECT * FROM usuarios
-        WHERE usuario = '{$email}' AND senha = '{$senha}'";
+    $sql = "SELECT * FROM cadastro_clientes
+        WHERE email = '{$email}' AND senha = '{$senha}'";
 
-$res = $conexao->query($sql) or die($conexao->error);
+    $res = $conexao->query($sql) or die($conexao->error);
 
-$row = $res->fetch_object();
+    $row = $res->fetch_object();
 
-$qtd = $res->num_rows;
+    $qtd = $res->num_rows;
 
-if ($qtd > 0) {
-    $_SESSION["email"] = $email;
-    $_SESSION["senha"] = $senha;
-    header('Location: index.html');
-    exit;
-} else {
-    echo "<script>alert('Usuário e/ou senha incorretos');</script>";
+    if ($qtd > 0) {
+        $_SESSION["email"] = $email;
+        $_SESSION["senha"] = $senha;
+        header('Location: checkout.html');
+        exit;
+    } else {
+        $_SESSION['erro_login'] = "Usuário e/ou senha incorretos";
+        header("Location: cadastro.php");  // Redireciona para a página de cadastro
+        exit;
+    }
 }
 ?>
